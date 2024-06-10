cb363fef6389cd178c91baaae5dddbc9c940f337
Sergio González <sgzzsifuentes@gmail.com>
2024-06-10 15:30:19 -0600

    Add suggestedUsers endpoint

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -80,3 +80,40 @@ export const followUnfollowUser = async (req, res) => {
         res.status(500).json({ error: error.message });
     }
 }
+
+export const getSuggestedUsers = async (req, res) => {
+    try
+    {
+        const userId = req.user._id;
+
+        const usersFollowedByMe = await User.findById(userId).select("following");
+
+        const users = await User.aggregate([
+            {
+                $match: {
+                    _id: {$ne: userId}
+                }
+            },
+            {
+                $sample: {size:10}
+            }
+        ])
+
+        // Exclude users already followed by logged in user
+        const filteredUsers = users.filter(user => !usersFollowedByMe.following.includes(user._id));
+
+        // Get only 4 values
+        const suggestedUsers = filteredUsers.slice(0,4);
+
+        // Users are retrieved with password, so let's get rid of it
+        suggestedUsers.forEach(user=>user.password=null);
+
+        res.status(200).json(suggestedUsers);
+    }
+    catch (error)
+    {
+        console.log("Error in getSuggestedUsers: ", error.message);
+        
+        res.status(500).json({ error: error.message });
+    }
+}
