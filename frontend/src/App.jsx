ab676df2e47279dcc00554b19ffffc9fe6140c26
Sergon <sgzzsifuentes@gmail.com>
2024-06-24 16:22:03 -0600

    Start routing of frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
+
+import HomePage from './pages/home/HomePage.jsx'
+import LoginPage from './pages/auth/login/LoginPage.jsx';
+import SignUpPage from './pages/auth/signup/SignUpPage';
+
+function App() {
+  return (
+    <div className='flex max-w-6xl mx-auto'>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/signup' element={<SignUpPage />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App
