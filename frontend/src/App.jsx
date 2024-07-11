69997a62dda64429ee6d8adf35bea6f61926d628
Sergon <sgzzsifuentes@gmail.com>
2024-07-11 10:57:49 -0600

    Add hardcoded Posts to populate Post page and Post page itself

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,10 +3,13 @@ import { Route, Routes } from 'react-router-dom';
 import HomePage from './pages/home/HomePage.jsx'
 import LoginPage from './pages/auth/login/LoginPage.jsx';
 import SignUpPage from './pages/auth/signup/SignUpPage';
+import Sidebar from './components/common/Sidebar.jsx';
 
 function App() {
   return (
     <div className='flex max-w-6xl mx-auto'>
+      {/* Common component*/}
+      <Sidebar />
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/login' element={<LoginPage />} />
---
+++ frontend/src/utils/db/dummy.js
@@ -0,0 +1,116 @@
+export const POSTS = [
+	{
+		_id: "1",
+		text: "¡Muchas gracias por todos los saludos y felicitaciones!! 😃 Otro año que coincide mi cumple estando con la Selección… Así que festejando un poco nomás y seguimos preparándonos para nuestro próximo partido. ¡Abrazos!",
+		img: "/posts/champs.png",
+		user: {
+			username: "messi",
+			profileImg: "/avatars/messi.png",
+			fullName: "Leo Messi",
+		},
+		comments: [
+			{
+				_id: "1",
+				text: "GOAT",
+				user: {
+					username: "korra",
+					profileImg: "/avatars/korra.png",
+					fullName: "Korra",
+				},
+			},
+		],
+		likes: ["6658s891", "6658s892", "6658s893", "6658s894"],
+	},
+	{
+		_id: "2",
+		text: "How you guys doing? 😊",
+		user: {
+			username: "messi",
+			profileImg: "/avatars/messi.png",
+			fullName: "Leo Messi",
+		},
+		comments: [
+			{
+				_id: "1",
+				text: "Best in the world",
+				user: {
+					username: "korra",
+					profileImg: "/avatars/korra.png",
+					fullName: "Korra",
+				},
+			},
+		],
+		likes: ["6658s891", "6658s892", "6658s893", "6658s894"],
+	},
+	{
+		_id: "3",
+		text: "Primer paso.",
+		img: "/posts/paso.png",
+		user: {
+			username: "messi",
+			profileImg: "/avatars/messi.png",
+			fullName: "Leo Messi",
+		},
+		comments: [],
+		likes: ["6658s891", "6658s892", "6658s893", "6658s894", "6658s895", "6658s896"],
+	},
+	{
+		_id: "4",
+		text: "Eu Quero Tchuuuuu <3",
+		img: "/posts/duo.png",
+		user: {
+			username: "yamal",
+			profileImg: "/avatars/yamal.png",
+			fullName: "Lamine Yamal",
+		},
+		comments: [
+			{
+				_id: "1",
+				text: "Great game!",
+				user: {
+					username: "korra",
+					profileImg: "/avatars/korra.png",
+					fullName: "Korra",
+				},
+			},
+		],
+		likes: [
+			"6658s891",
+			"6658s892",
+			"6658s893",
+			"6658s894",
+			"6658s895",
+			"6658s896",
+			"6658s897",
+			"6658s898",
+			"6658s899",
+		],
+	},
+];
+
+export const USERS_FOR_RIGHT_PANEL = [
+	{
+		_id: "1",
+		fullName: "Leo Messi",
+		username: "messi",
+		profileImg: "/avatars/messi.png",
+	},
+	{
+		_id: "2",
+		fullName: "Lamine Yamal",
+		username: "yamal",
+		profileImg: "/avatars/yamal.png",
+	},
+	{
+		_id: "3",
+		fullName: "Aang",
+		username: "aang",
+		profileImg: "/avatars/aang.png",
+	},
+	{
+		_id: "4",
+		fullName: "Korra",
+		username: "korra",
+		profileImg: "/avatars/korra.png",
+	},
+];
