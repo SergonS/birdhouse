5f9da850da901bc4e9a49aec6987b3471d0c4064
Sergon <sgzzsifuentes@gmail.com>
2024-09-05 17:57:33 -0600

    Add tanstack for data fetching

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { QueryClient } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
+    <QueryClient client={queryClient}>
       <App />
+    </QueryClient>
     </BrowserRouter>
   </React.StrictMode>,
 )
