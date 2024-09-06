78db7839394508a2c8e1f5f198df46b7fa1efb64
Sergon <sgzzsifuentes@gmail.com>
2024-09-05 21:00:31 -0600

    Connect front to back in Login Page

--- frontend/src/pages/auth/login/LoginPage.jsx
+++ frontend/src/pages/auth/login/LoginPage.jsx
@@ -1,11 +1,17 @@
-import { useState } from "react";
+// React
 import { Link } from "react-router-dom";
-
-import XSvg from "../../../components/svgs/X";
-
+// Hooks
+import { useState } from "react";
+// Icons
 import { FaUser } from "react-icons/fa";
 import { MdOutlineMail } from "react-icons/md";
 import { MdPassword } from "react-icons/md";
+// Media
+import XSvg from "../../../components/svgs/X";
+// Tanstack
+import { useMutation } from '@tanstack/react-query';
+// Toast
+import toast from 'react-hot-toast';
 
 const LoginPage = () => {
 	const [formData, setFormData] = useState({
@@ -13,16 +19,52 @@ const LoginPage = () => {
 		password: "",
 	});
 
+	const { mutate, isPending, isError, error } = useMutation({
+		mutationFn: async({ username, password }) => {
+			try
+			{
+				// Post
+				const res = await fetch("/api/auth/login", {
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json"
+					},
+					body: JSON.stringify({ username, password })
+				});
+
+				// Receiving response
+				const data = await res.json();
+
+				// Check for errors
+				if (!res.ok)
+				{
+					throw new Error(data.error || "Failed to login");
+				}
+
+				return data;
+			}
+			catch (error)
+			{
+				console.error(error);
+				throw error;
+			}
+		},
+		onSuccess: () => {
+			toast.success("Login successful")
+		},
+	});
+
 	const handleSubmit = (e) => {
+		// Avoid page refresh
 		e.preventDefault();
-		console.log(formData);
+		// Submitting form
+		mutate(formData);
 	};
 
 	const handleInputChange = (e) => {
 		setFormData({ ...formData, [e.target.name]: e.target.value });
 	};
 
-	const isError = false;
 
 	return (
 		<div className='max-w-screen-xl mx-auto flex h-screen'>
@@ -52,8 +94,10 @@ const LoginPage = () => {
 							value={formData.password}
 						/>
 					</label>
-					<button className='btn rounded-full btn-primary text-white'>Login</button>
-					{isError && <p className='text-red-500'>Something went wrong</p>}
+					<button className='btn rounded-full btn-primary text-white'>
+						{isPending ? "Loading..." : "Login"}
+					</button>
+					{isError && <p className='text-red-500'>{error.message}</p>}
 				</form>
 				<div className='flex flex-col gap-1 mt-3'>
 					<p className='text-white text-lg'>{"Don't"} have an account?</p>
@@ -65,4 +109,5 @@ const LoginPage = () => {
 		</div>
 	);
 };
+
 export default LoginPage;
