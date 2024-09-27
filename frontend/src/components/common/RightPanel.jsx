73d8b24b6c6dc801c2e612df6f92d0d5b854584d
Sergon <sgzzsifuentes@gmail.com>
2024-09-26 19:37:20 -0600

    Connected front to back Right Panel

--- frontend/src/components/common/RightPanel.jsx
+++ frontend/src/components/common/RightPanel.jsx
@@ -1,9 +1,37 @@
+// React
 import { Link } from "react-router-dom";
+// Components
 import RightPanelSkeleton from "../skeletons/RightPanelSkeleton";
-import { USERS_FOR_RIGHT_PANEL } from "../../utils/db/dummy";
+// Tanstack
+import { useQuery } from "@tanstack/react-query";
 
 const RightPanel = () => {
-	const isLoading = false;
+
+	const { data: suggestedUsers, isLoading } = useQuery({
+		queryKey: ['suggestedUsers'],
+		queryFn: async() => {
+			try
+			{
+				// Get
+				const res = await fetch("/api/users/suggested");
+
+				// Receiving response
+				const data = await res.json();
+
+				// Check for errors
+				if (!res.ok)
+				{
+					throw new Error(data.error || "Failed to retrieve suggested users");
+				}
+
+				return data;
+			}
+			catch (error)
+			{
+				throw new Error(error);
+			}
+		}
+	})
 
 	return (
 		<div className='hidden lg:block my-4 mx-2'>
@@ -20,7 +48,7 @@ const RightPanel = () => {
 						</>
 					)}
 					{!isLoading &&
-						USERS_FOR_RIGHT_PANEL?.map((user) => (
+						suggestedUsers?.map((user) => (
 							<Link
 								to={`/profile/${user.username}`}
 								className='flex items-center justify-between gap-4'
