75e14326d3a0474970bf6de2458e2d0bf59389a9
Sergon <sgzzsifuentes@gmail.com>
2025-08-15 16:52:23 -0600

    Implement like functionality

--- frontend/src/components/common/Post.jsx
+++ frontend/src/components/common/Post.jsx
@@ -25,7 +25,7 @@ const Post = ({ post }) => {
 	const { data: authUser } = useQuery({ queryKey: ['authUser'] });
 
 	// Delete post 
-	const { mutate: deletePost, isPending } = useMutation({
+	const { mutate: deletePost, isPending: isDeleting } = useMutation({
 		mutationFn: async() => {
 			try
 			{
@@ -58,6 +58,48 @@ const Post = ({ post }) => {
 		}
 	});
 
+	const { mutate: likePost, isPending: isLiking } = useMutation({
+		mutationFn: async () => {
+			try
+			{
+				const res = await fetch(`/api/posts/like/${post._id}`, {
+					method: "POST",
+				});
+
+				// Receiving response
+				const data = await res.json();
+
+				// Check for errors
+				if (!res.ok)
+				{
+					throw new Error(data.error || "Failed to retrieve suggested users");
+				}
+
+				return data;
+			}
+			catch (error)
+			{
+				throw new Error(error);
+			}
+		},
+		onSuccess: (updatedLikes) => {
+			toast.success("Post liked successfully");
+			
+			queryClient.setQueryData(["posts"], (oldData) => {
+				return oldData.map((p) => {
+					if (p._id === post._id) 
+					{
+						return { ...p, likes: updatedLikes };
+					}
+					return p;
+				});
+			});
+		},
+		onError: (error) => {
+			toast.error(error.message);
+		}
+	})
+
 	const postOwner = post.user;
 
 	const isLiked = false;
@@ -76,7 +118,14 @@ const Post = ({ post }) => {
 		e.preventDefault();
 	};
 
-	const handleLikePost = () => {};
+	const handleLikePost = () => {
+		if (isLiking)
+		{
+			return;
+		}
+		
+		likePost();
+	};
 
 	return (
 		<>
@@ -98,8 +147,8 @@ const Post = ({ post }) => {
 						</span>
 						{isMyPost && (
 							<span className='flex justify-end flex-1'>
-								{!isPending && <FaTrash className='cursor-pointer hover:text-red-500' onClick={handleDeletePost} />}
-								{isPending && (
+								{!isDeleting && <FaTrash className='cursor-pointer hover:text-red-500' onClick={handleDeletePost} />}
+								{isDeleting && (
 									<LoadingSpinner size='sm'></LoadingSpinner>
 								)}
 							</span>
@@ -168,9 +217,7 @@ const Post = ({ post }) => {
 											onChange={(e) => setComment(e.target.value)}
 										/>
 										<button className='btn btn-primary rounded-full btn-sm text-white px-4'>
-											{isCommenting ? (
-												<span className='loading loading-spinner loading-md'></span>
-											) : (
+											{isCommenting ? <LoadingSpinner size="md" /> : (
 												"Post"
 											)}
 										</button>
@@ -185,10 +232,11 @@ const Post = ({ post }) => {
 								<span className='text-sm text-slate-500 group-hover:text-green-500'>0</span>
 							</div>
 							<div className='flex gap-1 items-center group cursor-pointer' onClick={handleLikePost}>
-								{!isLiked && (
+								{isLiking && <LoadingSpinner size="sm" />}
+								{!isLiked && !isLiking && (
 									<FaRegHeart className='w-4 h-4 cursor-pointer text-slate-500 group-hover:text-pink-500' />
 								)}
-								{isLiked && <FaRegHeart className='w-4 h-4 cursor-pointer text-pink-500 ' />}
+								{isLiked && !isLiking && <FaRegHeart className='w-4 h-4 cursor-pointer text-pink-500 ' />}
 
 								<span
 									className={`text-sm text-slate-500 group-hover:text-pink-500 ${
